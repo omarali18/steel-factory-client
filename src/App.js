370005534ae95315da4cf91877864352759d4a6d
omarali18 <omarali1814000@gmail.com>
2022-07-01 08:50:25 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import { Typography } from '@mui/material';
 import Header from './pages/Shared/Header/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Client from './pages/Client/Client';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header></Header>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="home" element={<Home />} />
+      <Route path="client" element={<Client />} />
+    </Routes>
+  </BrowserRouter>
+      
      <Typography variant="h3" sx={{color:"green"}}>
        omar ali
      </Typography>
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
+import "./Header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark, faBars } from '@fortawesome/free-solid-svg-icons'
+import { Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const xmark = <FontAwesomeIcon icon={faXmark} />
+const bars = <FontAwesomeIcon icon={faBars} />
 
 const Header = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <div>
-            <h3>This is header</h3>
+
+        <div className="Navbar">
+        <span className="nav-logo">DevLHB</span>
+        <div className={`nav-items ${isOpen && "open"}`}>
+            <Link className='link' onClick={() => setIsOpen(!isOpen)} to="/home">Home</Link>
+            <Link className='link' onClick={() => setIsOpen(!isOpen)} to="/client">Client</Link>
+            <Link className='link' onClick={() => setIsOpen(!isOpen)} to="/home">Home</Link>
+            <Link className='link' onClick={() => setIsOpen(!isOpen)} to="/client">Client</Link>
+          {/* <a href="/service">Service</a>
+          <a href="/contact">Contact</a> */}
         </div>
+        <div
+          className={`nav-toggle ${isOpen && "open"}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <div className="bar">{isOpen ? xmark : bars}</div>
+        </div>
+      </div>
+        // <div className='header'>
+        //     <ul>
+        //         <li><Link className='link' to="/home">Home</Link></li>
+        //         <li><Link className='link' to="/client">Client</Link></li>
+        //         <li><Link className='link' to="/client">Client</Link></li>
+        //         <li><Link className='link' to="/home">Dashbord</Link></li>
+        //         <li><Link className='link' to="/home">Dashbord</Link></li>
+        //     </ul>
+        //     <button className='bars'>{bars}</button>
+        //     {/* <h1>{xmark}</h1> */}
+        // </div>
     );
 };
 
